f12951107b189b4d52329b5dba9512d1e721ef83
jhdelca1 <jherna21@gmail.com>
2025-03-23 13:16:59 +0100

    Create vars.tf

---
+++ app-persistente/terraform/vars.tf
@@ -0,0 +1,16 @@
+variable "resource_group_name" {
+  description = "Nombre del grupo de recursos"
+  type        = string
+  default     = "rg-devops-practice"
+}
+
+variable "location" {
+  description = "Ubicación en Azure"
+  type        = string
+  default     = "West Europe"
+}
+
+variable "acr_name" {
+  description = "Nombre del Azure Container Registry"
+  type        = string
+}
